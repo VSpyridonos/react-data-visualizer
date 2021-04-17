ee32cc5c96ddf364edf6c6f7a97b3ab0f81b7775
VSpyridonos <vasilissp96@gmail.com>
2021-04-17 14:25:36 +0300

    Add graph axes & README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Data visualizer
+Visualizing data from [Gapminder]: https://www.gapminder.org/ data sets with React, D3 and MySQL.
--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -83,11 +83,11 @@ function ResponsiveDrawer({ window }) {
     );
 
 
-    // *********************** D3 ******************************
+    // *********************** D3 stuff***************************
     const width = 700;
     const height = 350;
 
-    const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+    const margin = { top: 20, right: 20, bottom: 20, left: 80 };
 
     const innerHeight = height - margin.top - margin.bottom;
     const innerWidth = width - margin.left - margin.right;
@@ -100,7 +100,7 @@ function ResponsiveDrawer({ window }) {
         .domain([0, max(data, d => parseFloat(d.money))])
         .range([0, innerWidth]);
 
-
+    // ***********************************************************
 
 
     const handleSubmit = async (e) => {
@@ -243,21 +243,54 @@ function ResponsiveDrawer({ window }) {
             <main className={classes.content}>
                 <div className={classes.toolbar} />
 
-
                 <br />
 
                 <div>
                     <svg width={width} height={height}>
                         <g transform={`translate(${margin.left},${margin.top})`}>
+                            {xScale.ticks().map(tickValue => (
+                                <g key={tickValue} transform={`translate(${xScale(tickValue)}, 0)`}>
+                                    <line
+                                        x1={0}
+                                        y1={0}
+                                        x2={0}
+                                        y2={innerHeight}
+                                        stroke="black">
+                                    </line>
+                                    <text
+                                        dy=".71em"
+                                        style={{ textAnchor: 'middle' }}
+                                        y={innerHeight + 3}
+                                    >
+                                        {tickValue}
+                                    </text>
+                                </g>
+                            ))}
+
+                            {yScale.domain().map(tickValue => (
+
+                                <text
+                                    key={tickValue}
+                                    dy=".32em"
+                                    x="-3"
+                                    y={yScale(tickValue) + yScale.bandwidth() / 2}
+                                    style={{ textAnchor: 'end' }}
+                                >
+                                    {tickValue}
+                                </text>
+
+                            ))}
+
                             {data.map(d => (
                                 <rect
-                                    key={d.id}
+                                    key={d.country}
                                     x={0}
                                     y={yScale(d.country)}
                                     width={xScale(parseFloat(d.money))}
                                     height={yScale.bandwidth()}
                                 />
                             ))}
+
                         </g>
                     </svg>
                 </div>
